62ea825af44fc93e3dff0b88c8043c943712e408
Rehan <rehanadipurwana@gmail.com>
2023-09-01 15:18:01 +0700

    Fix error message

--- lib/utils/error.ts
+++ lib/utils/error.ts
@@ -1,6 +1,7 @@
 export const errorMessage = (err: any) => {
   const message = err instanceof Error ? err.message : 'Unknown error'
-  if (err! instanceof Error) console.error(err)
-  console.log(`❌ Error message: ${message}`)
+  // if (err! instanceof Error) console.log(err)
+  // console.log(`❌ Error message: ${message}`)
+  console.log(`❌ Error: ${err}`)
   return message
 }
